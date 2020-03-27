57ac6d39ccbb2312628b9094a332df82da7e1e59
yashvardhan18 <Yadavyash18>
2020-03-27 20:38:05 +0530

    Required classes for Problem4.

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,33 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+// use problem1.mybst.MyBinarySearchobj class for creating binary obj
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree obj = new MyBinarySearchTree();
+        //taking hard-coded inputs
+        obj.add(51);
+        obj.add(39);
+        obj.add(31);
+        obj.add(54);
+        obj.add(92);
+        obj.add(42);
+        obj.add(21);
+        obj.add(10);
+        obj.add(26);
+        obj.add(52);
+        obj.add(36);
+        obj.add(47);
+        obj.add(82);
+        obj.add(5);
+        obj.add(62);
+        MyQueue queue = new MyQueue();
+        queue.entry(obj.root);
+        queue.printQ();
+    }
 }
--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -5,7 +5,50 @@
  *  Time: 7:11 PM
  */
 package problem4.myqueue;
-// to create queue to store pre - order successor
+
+import problem1.node.TreeNode;
+import problem4.node.Node;
+
+import static problem1.mybst.MyBinarySearchTree.preOrder;
+import static problem1.mybst.MyBinarySearchTree.preString;
 
 public class MyQueue {
+    private Node rear;
+    private Node front;
+    private int size;
+
+    public MyQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+    public void entry(TreeNode root) {
+        preOrder(root);
+        String[] data = preString.toString().split(" ");
+        for (int i = 0; i < data.length; i++) {
+            Node node = new Node(Integer.parseInt(data[i]));
+            if (front == null) {
+                rear = node;
+                front = node;
+                size++;
+            } else {
+                rear.setNext(node);
+                rear = node;
+                size++;
+            }
+        }
+    }
+
+    public void printQ() {
+        Node temp = this.front.getNext();
+        for (int i = 0; i < this.size - 1; i++) {
+            if (i != this.size - 2) {
+                System.out.print(temp.getData() + "->");
+                temp = temp.getNext();
+            } else {
+                System.out.println(temp.getData());
+            }
+        }
+    }
 }
