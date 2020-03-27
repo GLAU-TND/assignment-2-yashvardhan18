6829d096f47d7860acf4faeb430b522a37637afb
yashvardhan18 <Yadavyash18>
2020-03-27 19:42:53 +0530

    Created main class for problem 1.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree ob = new MyBinarySearchTree();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("No. of inputs:-");
+        int i = sc.nextInt();
+        while (i > 0) {
+            ob.add(sc.nextInt());
+        }
+    }
 }
