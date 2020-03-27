34204d4025545a83320f126ccf63b5015522458e
yashvardhan18 <Yadavyash18>
2020-03-27 20:25:27 +0530

    Required lasses for problem 3 .

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,20 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+    public static void main(String[] args) {
+        MyPriorityQueue obj = new MyPriorityQueue();
+        obj.entry(new Node(new Student(15, "wdad")));
+        obj.entry(new Node(new Student(500, "wdad")));
+        obj.entry(new Node(new Student(50, "wdad")));
+        obj.entry(new Node(new Student(11, "wdad")));
+        obj.entry(new Node(new Student(55, "wdad")));
+        obj.printQ();
+    }
 }
--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -5,7 +5,31 @@
  *  Time: 7:13 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+    private Student student;
+    private Node next;
+
+    public Node(Student student) {
+        this.student = student;
+        next = null;
+    }
+
+    public Student getData() {
+        return student;
+    }
+
+    public void setData(Student student) {
+        this.student = student;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 
 }
